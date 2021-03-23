32911cec475e0663ee0d6017a9c4be13405ca815
XY2336661 <751529716@qq.com>
2021-03-23 18:55:49 +0800

    创建第一个类

---
+++ students/soft1914080902251/Soft1914080902251Activity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidlabs.soft1914080902251;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.hzuapps.androidlabs.R;
+
+public class Soft1914080902251Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1914080902251);
+    }
+}
